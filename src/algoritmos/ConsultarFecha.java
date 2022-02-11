404c2df8ac5006411c4177a228c9c80c57a7429e
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-11 15:37:08 -0500

    Ejercicio 13, consulta de fecha en formato (AAAA/MM/DD) (HH:MM:SS) - ajustes

--- src/algoritmos/ConsultarFecha.java
+++ src/algoritmos/ConsultarFecha.java
@@ -1,11 +1,9 @@
 package algoritmos;
 
 import java.text.SimpleDateFormat;
-import java.util.Scanner;
 import java.util.Date;
 public class ConsultarFecha {
     public static void ej13() {
-        Scanner sc=new Scanner(System.in);
         System.out.println("Ejercicio 13: Consultar fecha");
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
