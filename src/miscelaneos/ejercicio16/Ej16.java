7e8403f994dc804aec1dbcc01fd09fa95aa1dac2
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-11 20:38:45 -0500

    Programación de salidas para muestras de resultados de mayoría de edad y conclusión respecto al peso

--- src/miscelaneos/ejercicio16/Ej16.java
+++ src/miscelaneos/ejercicio16/Ej16.java
@@ -1,7 +1,58 @@
 package miscelaneos.ejercicio16;
 
+import java.util.Scanner;
+
 public class Ej16 {
     public static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese nombre: ");
+        String nom=sc.nextLine();
+        System.out.print("Ingrese edad: ");
+        int edad=sc.nextInt();
+        sc.nextLine();
+        System.out.print("Ingrese sexo: ");
+        String sexo=sc.nextLine();
+        System.out.print("Ingrese peso: ");
+        double peso=Double.parseDouble(sc.nextLine());
+        System.out.print("Ingrese altura: ");
+        double altura=Double.parseDouble(sc.nextLine());
+        System.out.println("------------------------------------");
+
+        Persona p1 = new Persona(nom,edad,sexo.charAt(0),peso,altura);
+        Persona p2 = new Persona(nom,edad,sexo.charAt(0));
+        Persona p3 = new Persona();
+
+
+        mostrarIMC(p1);
+        System.out.println(p1);
+        System.out.println("Es mayor de edad: "+p1.isMayorDeEdad());
+        System.out.println();
+
+        mostrarIMC(p2);
+        System.out.println(p2);
+        System.out.println("Es mayor de edad: "+p2.isMayorDeEdad());
+        System.out.println();
+
+        mostrarIMC(p3);
+        System.out.println(p3);
+        System.out.println("Es mayor de edad: "+p3.isMayorDeEdad());
+        System.out.println("------------------------------------");
+
+    }
+
+    public static void mostrarIMC(Persona p){
+        switch(p.calcularIMC()){
+            case Persona.PESO_ALTO:
+                System.out.println("La siguiente persona tiene sobrepeso");
+                break;
+            case Persona.PESO_BAJO:
+                System.out.println("La siguiente persona tiene bajo peso");
+                break;
+            case Persona.PESO_IDEAL:
+                System.out.println("La siguiente persona tiene peso ideal");
+                break;
+
+        }
 
     }
 }
--- src/miscelaneos/ejercicio16/Persona.java
+++ src/miscelaneos/ejercicio16/Persona.java
@@ -7,6 +7,9 @@ import java.util.stream.Collectors;
 public class Persona {
     //Constantes
     private final char SEX_DEF='H';
+    public final static int PESO_BAJO=-1;
+    public final static int PESO_IDEAL=0;
+    public final static int PESO_ALTO=1;
 
     //Atributos
     private String nombre;
@@ -31,49 +34,98 @@ public class Persona {
         this.edad=edad;
         this.dni=generarDNI();
         this.sexo=sexo;
+        revisarSexo(sexo);
         this.peso=0.0;
         this.altura=0.0;
+
     }
 
-    public Persona(String nombre, int edad, String dni, char sexo, double peso, double altura) {
+    public Persona(String nombre, int edad,  char sexo, double peso, double altura) {
         this.nombre = nombre;
         this.edad = edad;
-        this.dni = dni;
+        this.dni = generarDNI();
         this.sexo = sexo;
+        revisarSexo(sexo);
         this.peso = peso;
         this.altura = altura;
     }
 
     public int calcularIMC(){
-        double imc = this.peso/(Math.pow(this.altura,2));
-        if(imc<20){
-            return -1;
-        }else if(imc<25 && imc>20){
-            return 0;
+        double imc = peso/(Math.pow(altura,2));
+        if(peso == 0){
+            return PESO_BAJO;
+        }else{
+            if(imc<20){
+                return PESO_BAJO;
+            }else if(imc<=25 && imc>=20){
+                return PESO_IDEAL;
+            }
+            return PESO_ALTO;
         }
-        return 1;
+
 
     }
 
     public void revisarSexo(char sexo){
-        if(!(sexo=='H' || sexo=='M')){
+        if(sexo!='H' && sexo!='M'){
             this.sexo=SEX_DEF;
         }
     }
 
     public String generarDNI(){
-        String secNums = Arrays.stream(generarSecuenciaNums())
-                .mapToObj(String::valueOf)
-                .collect(Collectors.joining(""));
-        return this.sexo+secNums;
+        int secNums=generarSecuenciaNums();
+        char letra=generaLetraDNI((int) Math.floor(Math.random()*26));
+
+        return Integer.toString(secNums)+letra;
+
     }
 
-    public int [] generarSecuenciaNums(){
-        int [] sec = new int[8];
-        Random r = new Random();
-        for (int i=0; i<sec.length;i++){
-            sec[i]=r.nextInt(10);
-        }
-        return sec;
+    public int  generarSecuenciaNums(){
+        return ((int) Math.floor(Math.random() * (100000000 - 10000000) + 10000000));
+
+    }
+
+    private char generaLetraDNI(int res) {
+        char [] letras = {'A','B','C','D','E','F','G','H','I',
+                          'J','K','L','M','N','O','P','Q','R',
+                          'S','T','U','V','W','X','Y','Z'};
+
+        return letras[res];
+    }
+
+    public boolean isMayorDeEdad(){
+        return edad>=18;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Informacion: \n" +
+                "\tNombre: "+nombre+"\n" +
+                "\tDNI: "+dni+"\n" +
+                "\tEdad: "+edad+"\n" +
+                "\tSexo: "+sexo+"\n" +
+                "\tPeso: "+peso+"\n" +
+                "\tAltura: "+altura;
+
     }
 }
